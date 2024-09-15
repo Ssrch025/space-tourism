ed904ea21404a586290909089e39a07a74f4cd8f
Ssrch025 <rungsuriya20707@hotmail.co.th>
2024-09-15 15:43:57 +0700

    master: fix responsive size from position to flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,57 +4,63 @@ import Button from "@mui/material/Button"
 import { useTheme } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
 
-
 export default function Home() {
   const theme = useTheme()
+
   return (
     <Stack
       sx={{
-        position: 'relative',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         width: '100%',
         minHeight: { xs: '85vh', md: '70vh' },
+        marginY: 'auto',
+        paddingTop: 10,
+        [theme.breakpoints.up(1024)]: {
+          paddingX: 15,
+          flexDirection: 'row'
+        },
       }}
     >
       <Stack
         sx={{
-          [theme.breakpoints.up('xs')]: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            textAlign: 'center',
-            m: 'auto',
-            width: '70%'
-          },
-          [theme.breakpoints.up('sm')]: {
-            width: '50%'
-          },
-          [theme.breakpoints.up('md')]: {
-            position: 'absolute',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+          width: { xs: '70%', sm: '50%' },
+          [theme.breakpoints.up(1024)]: {
             alignItems: 'flex-start',
-            textAlign: 'start',
-            width: '30%',
-            bottom: 0,
-            left: 120,
-          },
-          [theme.breakpoints.up('lg')]: {
-            left: 165,
+            textAlign: 'left',
           },
         }}
       >
-        <Typography variant="heading5" color='space.purple' >
+        <Typography variant="heading5" color='space.purple'>
           {'SO, YOU WANT TO TRAVEL TO'}
         </Typography>
-        <Typography variant="heading1" color='space.white' >
+        <Typography variant="heading1" color='space.white'>
           {'SPACE'}
         </Typography>
-        <Typography variant="bodytext" color='space.purple' sx={{ wordBreak: 'break-word' }}  >
+        <Typography variant="bodytext" color='space.purple' sx={{ wordBreak: 'break-word' }}>
           {`Let’s face it; if you want to go to space, you might as well
         genuinely go to outer space and not hover kind of on the
         edge of it. Well sit back, and relax because we’ll give you a
         truly out of this world experience!`}
         </Typography>
       </Stack>
-      <Stack sx={{ width: '100%', minHeight: '25vh', display: 'flex', alignItems: 'center' }}>
+      <Stack
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          minHeight: '25vh',
+          [theme.breakpoints.up(1024)]: {
+            alignItems: 'flex-end'
+          },
+        }}
+      >
         <Button
           variant="contained"
           sx={{
@@ -62,27 +68,17 @@ export default function Home() {
             bgcolor: 'space.white',
             width: { xs: 150, sm: 242, md: 274 },
             height: { xs: 150, sm: 242, md: 274 },
-            position: 'absolute',
-            [theme.breakpoints.up('md')]: {
-              position: 'absolute',
-              bottom: 0,
-              right: 120,
-            },
-            [theme.breakpoints.up('lg')]: {
-              right: 165,
-              bottom: 35,
-            },
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
             '&:hover': {
               bgcolor: 'rgba(255, 255, 255, 1)',
               backdropFilter: 'blur(10px)',
-              boxShadow: '0px 0px 0px 60px rgba(255, 255, 255, 0.15)'
-            }
+              boxShadow: '0px 0px 0px 60px rgba(255, 255, 255, 0.15)',
+            },
           }}
         >
-          <Typography
-            variant="heading4"
-            color={'space.darkNavy'}
-          >
+          <Typography variant="heading4" color={'space.darkNavy'}>
             {'EXPLORE'}
           </Typography>
         </Button>
--- src/components/StyledLayout.tsx
+++ src/components/StyledLayout.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 
 const StyledLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Box sx={{ position: 'relative', height: '100vh', width: '100%' }}>
+        <Box sx={{ position: 'relative', minHeight: '100vh', width: '100%' }}>
             <Box
                 sx={{
                     position: 'absolute',
@@ -15,8 +15,9 @@ const StyledLayout = ({ children }: { children: React.ReactNode }) => {
                     },
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
+                    backgroundAttachment: 'fixed',
                     width: '100%',
-                    height: '100vh',
+                    minHeight: '100vh',
                 }}
             />
             <Box sx={{ position: 'absolute', width: '100%', marginTop: { xs: 0, md: 4 } }}>
